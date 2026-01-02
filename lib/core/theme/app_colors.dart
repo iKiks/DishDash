725be5306438122603c4988ef52275d8bffa5d0b
iKiks <ikiks.lanre@gmail.com>
2026-01-02 23:41:43 +0100

    Colors created

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+/// Central color palette for the app UI.
+class AppColors {
+	static const Color white = Color(0xFFFFFDF9);
+	static const Color pink = Color(0xFFFFC6C9);
+	static const Color sweetPink = Color(0xFFEC888D);
+	static const Color redPink = Color(0xFFFD5D69);
+	static const Color brownPod = Color(0xFF3E2823);
+	static const Color black = Color(0xFF1C0F0D);
+}
