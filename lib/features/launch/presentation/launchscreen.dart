b44d368cf11924a95278deb91cc94217299487ec
iKiks <ikiks.lanre@gmail.com>
2026-01-03 22:53:58 +0100

    launch screen animation created

--- lib/features/launch/presentation/launchscreen.dart
+++ lib/features/launch/presentation/launchscreen.dart
@@ -2,26 +2,73 @@ import 'package:dishdash/core/theme/app_colors.dart';
 import 'package:dishdash/core/widgets/texts/app_texts.dart';
 import 'package:flutter/material.dart';
 
-class Launchscreen extends StatelessWidget {
+class Launchscreen extends StatefulWidget {
   const Launchscreen({super.key});
+
+  @override
+  State<Launchscreen> createState() => _LaunchscreenState();
+}
+
+class _LaunchscreenState extends State<Launchscreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _scale;
+  late final Animation<double> _fade;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1200),
+    );
+
+    _scale = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOutBack,
+    );
+
+    _fade = CurvedAnimation(
+      parent: _controller,
+      curve: const Interval(0.2, 1.0, curve: Curves.easeIn),
+    );
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.redPink,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('lib/core/assets/logo.png', width: 150, height: 150),
-            const SizedBox(height: 20),
-            AppTexts(
-              'DishDash',
-              fontSize: 63.5,
-              color: Colors.white,
-              fontWeight: FontWeight.w600,
+        child: ScaleTransition(
+          scale: _scale,
+          child: FadeTransition(
+            opacity: _fade,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'lib/core/assets/logo.png',
+                  width: 150,
+                  height: 150,
+                ),
+                const SizedBox(height: 20),
+                AppTexts(
+                  'DishDash',
+                  fontSize: 63.5,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
