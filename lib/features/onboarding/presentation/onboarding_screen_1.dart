87829dcd38be9379c33f1c0cc97b64b14422d36e
iKiks <ikiks.lanre@gmail.com>
2026-01-04 23:51:11 +0100

    onboarding screen updated

--- lib/features/onboarding/presentation/onboarding_screen_1.dart
+++ lib/features/onboarding/presentation/onboarding_screen_1.dart
@@ -5,6 +5,10 @@ class OnboardingScreen1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Text('Onboarding Screen 1'),
+      ),
+    );
   }
 }
