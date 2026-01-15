294fb2ff3cb56818bcc6ab74c29fd05e82db53a6
iKiks <ikiks.lanre@gmail.com>
2026-01-15 23:09:11 +0100

    user feature updated

--- lib/core/storage/hive_storage.dart
+++ lib/core/storage/hive_storage.dart
@@ -1,3 +1,11 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
 class HiveStorage {
+    static const String _authBox = 'auth_box';
+
+    static Future<void> init() async {
+    await Hive.openBox(_authBox);
+  }
+
   
 }
