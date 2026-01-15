c0a4a07fbca43090dbcb22b0fdac47416231ce84
iKiks <ikiks.lanre@gmail.com>
2026-01-15 23:18:43 +0100

    user feature updated

---
+++ lib/core/common/entities/user.dart
@@ -0,0 +1,15 @@
+class User {
+  final String id;
+  final String name;
+  final String email;
+  final String? phoneNumber;
+  final DateTime? dateOfBirth;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.email,
+    this.phoneNumber,
+    this.dateOfBirth,
+  });
+}
