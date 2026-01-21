62e223e455ef803a8d55afebb4310e0e9649ef93
iKiks <ikiks.lanre@gmail.com>
2026-01-21 23:05:04 +0100

    Use AppLogger in SignUpUseCase

--- lib/features/login_sign_up/domain/usecases/sign_up_usecase.dart
+++ lib/features/login_sign_up/domain/usecases/sign_up_usecase.dart
@@ -1,5 +1,6 @@
 import 'package:dishdash/core/common/entities/user.dart';
 import 'package:dishdash/core/common/usecases/usecase.dart';
+import 'package:dishdash/core/utils/app_logger.dart';
 import 'package:dishdash/features/login_sign_up/domain/repositories/auth_repository.dart';
 
 class SignUpParams {
@@ -16,7 +17,7 @@ class SignUpUseCase extends UseCase<void, SignUpParams> {
 
   @override
   Future<void> call(SignUpParams params) {
-    print('[SignUpUseCase] Executing for ${params.user.email}');
+    AppLogger.d('[SignUpUseCase] Executing for ${params.user.email}');
     return _repository.signUp(user: params.user, password: params.password);
   }
 }
