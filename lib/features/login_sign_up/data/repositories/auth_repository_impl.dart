e92a2073a04f4e0c829c8095b1c6ef72f82b6a2b
iKiks <ikiks.lanre@gmail.com>
2026-01-21 22:56:49 +0100

    Replace print with AppLogger in auth repository

--- lib/features/login_sign_up/data/repositories/auth_repository_impl.dart
+++ lib/features/login_sign_up/data/repositories/auth_repository_impl.dart
@@ -1,4 +1,5 @@
 import 'package:dishdash/core/common/entities/user.dart';
+import 'package:dishdash/core/utils/app_logger.dart';
 import 'package:dishdash/features/login_sign_up/data/datasources/remote_datasource.dart';
 import 'package:dishdash/features/login_sign_up/domain/repositories/auth_repository.dart';
 
@@ -10,13 +11,15 @@ class AuthRepositoryImpl implements AuthRepository {
 
   @override
   Future<String> login({required String email, required String password}) {
-    print('[AuthRepositoryImpl.login] Forwarding login for $email');
+    AppLogger.d('[AuthRepositoryImpl.login] Forwarding login for $email');
     return _remoteDatasource.login(email, password);
   }
 
   @override
   Future<void> signUp({required User user, required String password}) {
-    print('[AuthRepositoryImpl.signUp] Forwarding sign up for ${user.email}');
+    AppLogger.d(
+      '[AuthRepositoryImpl.signUp] Forwarding sign up for ${user.email}',
+    );
     return _remoteDatasource.signUp(user, password);
   }
 }
