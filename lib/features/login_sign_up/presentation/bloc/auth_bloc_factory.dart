27696f7dd8f1ab55b4025ec0a45cebb0a2360212
iKiks <ikiks.lanre@gmail.com>
2026-01-21 23:05:34 +0100

    Use AppLogger in AuthBlocFactory

--- lib/features/login_sign_up/presentation/bloc/auth_bloc_factory.dart
+++ lib/features/login_sign_up/presentation/bloc/auth_bloc_factory.dart
@@ -1,5 +1,6 @@
 import 'package:dishdash/core/constants/app_config.dart';
 import 'package:dishdash/core/network/api_client.dart';
+import 'package:dishdash/core/utils/app_logger.dart';
 import 'package:dishdash/features/login_sign_up/data/datasources/remote_datasource.dart';
 import 'package:dishdash/features/login_sign_up/data/repositories/auth_repository_impl.dart';
 import 'package:dishdash/features/login_sign_up/domain/usecases/login_usecase.dart';
@@ -11,9 +12,9 @@ class AuthBlocFactory {
   const AuthBlocFactory._();
 
   static AuthBloc create() {
-    print('[AuthBlocFactory] Creating AuthBloc');
+    AppLogger.d('[AuthBlocFactory] Creating AuthBloc');
     final apiClient = ApiClient(baseUrl: AppConfig.apiBaseUrl);
-    print(
+    AppLogger.d(
       '[AuthBlocFactory] ApiClient initialized with baseUrl ${AppConfig.apiBaseUrl}',
     );
     final remoteDatasource = RemoteDatasourceImpl(apiClient: apiClient);
