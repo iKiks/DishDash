30ea16b4b07b45d76f5e15f8964bfd3857e7a67b
iKiks <ikiks.lanre@gmail.com>
2026-01-21 23:02:42 +0100

    Use AppLogger in LoginUseCase

--- lib/features/login_sign_up/domain/usecases/login_usecase.dart
+++ lib/features/login_sign_up/domain/usecases/login_usecase.dart
@@ -1,4 +1,5 @@
 import 'package:dishdash/core/common/usecases/usecase.dart';
+import 'package:dishdash/core/utils/app_logger.dart';
 import 'package:dishdash/features/login_sign_up/domain/repositories/auth_repository.dart';
 
 class LoginParams {
@@ -15,7 +16,7 @@ class LoginUseCase extends UseCase<String, LoginParams> {
 
   @override
   Future<String> call(LoginParams params) {
-    print('[LoginUseCase] Executing with email ${params.email}');
+    AppLogger.d('[LoginUseCase] Executing with email ${params.email}');
     return _repository.login(email: params.email, password: params.password);
   }
 }
