6c22018ef89281f98fe84f8e6396d79deaa8b56d
iKiks <ikiks.lanre@gmail.com>
2026-01-25 23:12:56 +0100

    CategoryChips: inline chip padding (format)

--- lib/features/home_page/presentation/widgets/category_chips.dart
+++ lib/features/home_page/presentation/widgets/category_chips.dart
@@ -36,10 +36,7 @@ class CategoryChips extends StatelessWidget {
               onTap: () => onSelected(index),
               child: AnimatedContainer(
                 duration: const Duration(milliseconds: 180),
-                padding: EdgeInsets.symmetric(
-                  horizontal: ResponsiveSize.width(14),
-                  vertical: ResponsiveSize.height(8),
-                ),
+                padding: EdgeInsets.symmetric(horizontal: ResponsiveSize.width(14), vertical: ResponsiveSize.height(8)),
                 decoration: BoxDecoration(
                   color: isSelected ? AppColors.redPink : AppColors.transparent,
                   borderRadius: BorderRadius.circular(20),
