c3cc5ee2374286c8d2bffa4250653607b598be8a
iKiks <ikiks.lanre@gmail.com>
2026-01-25 23:10:19 +0100

    HomePage: inline top chef title padding (format)

--- lib/features/home_page/presentation/homepage.dart
+++ lib/features/home_page/presentation/homepage.dart
@@ -98,9 +98,7 @@ class _HomePageState extends State<HomePage> {
               ),
               SizedBox(height: ResponsiveSize.height(16)),
               Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: pageHorizontalPadding,
-                ),
+                padding: EdgeInsets.symmetric(horizontal: pageHorizontalPadding),
                 child: _buildSectionTitle('Top Chef'),
               ),
               SizedBox(height: ResponsiveSize.height(10)),
